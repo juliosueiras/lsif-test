fd7fd4a84104f2a09096608f2611b0abd8383674
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-02-20 17:46:58 -0500

    Updated

--- test.tf
+++ test.tf
@@ -1,8 +1,7 @@
-#locals {
-#  test = yamldecode(file("${path.module}/test.yaml"))
-#}
-
-resource "local_file" "DS" {
-  content = local.test
+locals {
+  test2 = var.test
 }
 
+variable "test" {
+  type = string
+}
